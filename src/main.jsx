59e8a21a36d8eabfa7d71beac88934043ecf3a90
knikhil07 <knikhil07k@gmail.com>
2025-01-05 17:07:17 +0530

    render

--- src/main.jsx
+++ src/main.jsx
@@ -7,10 +7,9 @@ import { BrowserRouter, HashRouter } from 'react-router-dom'
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     
- 
-    <HashRouter>
+    <BrowserRouter>
     <App />
-
-    </HashRouter>
+    </BrowserRouter>
+    
   </StrictMode>,
 )
